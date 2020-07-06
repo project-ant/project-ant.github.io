e4583dbf4d941aff53c83954e6a6bfd61975d3d1
project-ant <s21810297@student.unklab.ac.id>
2020-07-06 14:08:20 +0800

    Adding Project

---
+++ index.js
@@ -0,0 +1,69 @@
+let person = document.getElementById("person");
+
+person.onclick = showMessage;
+function showMessage() {
+    alert("I've been in this university for years.. And this is fun.");
+ }
+
+
+let education = document.getElementById("education");
+
+education.onclick = showMessage;
+function showMessage() {
+    alert("Thanks to the education from my elementary for spiritual purposes and Middle/High School for my English ability with public speaking");
+ }
+
+
+let awards = document.getElementById("awards");
+
+awards.onclick = showMessage;
+function showMessage() {
+    alert("When given the opportunity, I'll go do such task with wholeheartedly");
+ }
+
+
+let experience = document.getElementById("experience");
+
+experience.onclick = showMessage;
+function showMessage() {
+    alert("Many Things worth to learn, so none is quite few.");
+ }
+
+
+let contact = document.getElementById("contact");
+
+contact.onclick = showMessage;
+function showMessage() {
+    alert("Make sure to let me know in advance through my e-mail so I can confirm you're the one contacting.");
+ }
+
+
+let fun_fact1 = document.getElementById("fun_fact1");
+
+fun_fact1.onclick = showMessage;
+function showMessage() {
+    alert("As a gamer, I can only do support. No more, no Less");
+ }
+
+
+let fun_fact2 = document.getElementById("fun_fact2");
+
+fun_fact2.onclick = showMessage;
+function showMessage() {
+    alert("I really want to meet many people. Just meet, not memorize :P");
+ }
+
+
+let fun_fact3 = document.getElementById("fun_fact3");
+
+fun_fact3.onclick = showMessage;
+function showMessage() {
+    alert("I've been used to use my mask... that I can't possibly deny it");
+ }
+
+
+let fun_fact4 = document.getElementById("fun_fact4");
+
+fun_fact4.onclick = showMessage;function showMessage() {
+    alert("For the life of a person must be remembered... no matter who they are.  Btw, I'll keep udpate this in a month or two... when I'm free");
+ }
